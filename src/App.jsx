ff604479e5d76f216dacbe1354f58ac9bb12e185
KshithijKeshav <kshithijkeshav@gmail.com>
2024-05-17 01:14:40 +0530

    COMMIT 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AadharVerification from './components/AadharVerification';
+import Homepage from './components/Homepage';
 
 function App() {
+  const [isVerified, setIsVerified] = useState(false);
+
+  const handleVerificationSuccess = () => {
+    setIsVerified(true);
+  };
+
   return (
     <div>
       <p>
-        {/* Add the image before the heading */}
         <img
           src="https://th.bing.com/th?id=OIP.F_OGbA70Drg8vmiDoCYRTAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2"
           style={{ marginRight: '10px', width: '50px', height: '50px' }}
-          //alt="RVITM Logo"
+          alt="RVITM Logo"
         />
         <span style={{ fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif'}}>GOVERNMENT OF RVITM</span>
       </p>
-      <AadharVerification />
+      {isVerified ? <Homepage /> : <AadharVerification onSuccess={handleVerificationSuccess} />}
     </div>
   );
 }
 
 export default App;
+
